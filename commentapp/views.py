020af4ee7e609d0ab477f99618232b81ffd0e175
timgotango <xodzm3131@gmail.com>
2021-08-11 16:48:23 +0900

    8/11 못했던 commit

--- commentapp/views.py
+++ commentapp/views.py
@@ -23,6 +23,5 @@ class CommentDeleteView(DeleteView):
     model = Comment
     context_object_name = 'target_comment'
     template_name = 'commentapp/delete.html'
-
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
