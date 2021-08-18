11278dc58f85671967aad4a2a13a74f3100aa972
timgotango <xodzm3131@gmail.com>
2021-08-18 18:16:39 +0900

    8/18 commit

--- articleapp/models.py
+++ articleapp/models.py
@@ -2,12 +2,16 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
 
 class Article(models.Model):
     # onetoonekey는 1대1이였다면, foreignkey는 1대다
     # cascade(종속)가 user가 삭제되면 article 또한 삭제였다면, set_null은 user가 없어져도 신원 미상으로는 article 존재.
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
                                related_name='article', null=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL,
+                                related_name='article', null=True)
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)    # media 안에 article이라는 파일이 생기고 그 안에 이미지가 저장.
     content = models.TextField(null=True)    # 장문을 쓸 경우에는 textfield
